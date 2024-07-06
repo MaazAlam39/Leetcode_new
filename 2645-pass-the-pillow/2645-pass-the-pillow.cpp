b9b76399c15ca106956beac510808c67ca99e988
Maaz <102986485+MaazAlam39@users.noreply.github.com>
2024-07-07 00:03:37 +0530

    Time: 0 ms (100%), Space: 6.9 MB (100%) - LeetHub

--- 2645-pass-the-pillow/2645-pass-the-pillow.cpp
+++ 2645-pass-the-pillow/2645-pass-the-pillow.cpp
@@ -1,7 +1,12 @@
 class Solution {
 public:
     int passThePillow(int n, int time) {
-        int chunks=time/(n-1);
-        return chunks % 2 == 0 ? (time % (n - 1) + 1) : (n - time % (n - 1));
+        int completed=time/(n-1);
+        int remain=time%(n-1);
+        int ans=0;
+        if(completed % 2 !=0)ans=n-remain;
+        else ans=remain+1;
+        //return chunks % 2 == 0 ? (time % (n - 1) + 1) : (n - time % (n - 1));
+        return ans;
     }
 };
