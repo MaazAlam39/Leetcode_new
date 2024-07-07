1f82f558e5b520bcc033fb06009901123941f4a3
Maaz <102986485+MaazAlam39@users.noreply.github.com>
2024-07-07 23:39:10 +0530

    Time: 35 ms (69.04%), Space: 16.5 MB (22.7%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,8 +1,19 @@
+# class Solution:
+#     def plusOne(self, digits: List[int]) -> List[int]:
+#         for i in reversed(range(len(digits))):
+#             if digits[i]!=9:
+#                 digits[i]+=1
+#                 return digits
+#             digits[i]=0
+#         return [1]+digits
+
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        for i in reversed(range(len(digits))):
-            if digits[i]!=9:
-                digits[i]+=1
-                return digits
-            digits[i]=0
-        return [1]+digits
+        for i in range(len(digits)-1, -1, -1):
+            curr_digit = digits[i]
+            digits[i] = 1 + curr_digit
+
+            if curr_digit != 9: return digits
+            else: digits[i] = 0
+        
+        return [1] + digits
