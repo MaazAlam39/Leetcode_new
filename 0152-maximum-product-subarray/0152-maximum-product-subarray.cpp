2e6ee5fad28c6be3b20f4560a2043c1ba2f9612b
Maaz <102986485+MaazAlam39@users.noreply.github.com>
2024-09-19 15:39:54 +0530

    Time: 3 ms (85.36%), Space: 16.6 MB (45.18%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -5,8 +5,8 @@ public:
         for(int i=0;i<nums.size();i++){
             if(pre==0)pre=1;
             if(suf==0)suf=1;
-            pre=pre*nums[i];
-            suf=suf*nums[nums.size()-i-1];
+            pre*=nums[i];
+            suf*=nums[nums.size()-i-1];
             maxi=max(maxi,max(pre,suf));
         }
         return maxi;
