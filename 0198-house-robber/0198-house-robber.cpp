1d9d4d7e788127fc50925dd3df0a1f732aceb301
Maaz <102986485+MaazAlam39@users.noreply.github.com>
2024-09-29 21:00:17 +0530

    Time: 4 ms (32.11%), Space: 9.5 MB (63.76%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,20 +1,39 @@
 class Solution {
 public:
+    // int rob(vector<int>& nums) {
+    //     int n=nums.size();
+    //     vector<int> dp(n,-1);
+    //     int z=func(n-1,nums,dp);
+        
+    //     return z;
+        
+    // }
+    // int func(int ind,vector<int>&nums,vector<int>&dp){
+                                                     
+    //         if(ind==0)return nums[0];
+    //         if(ind<0)return 0; 
+    //         if(dp[ind]!=-1)return dp[ind];                                                                                                                                                                                                                                                                                                                                         
+    //         int pick=nums[ind]+func(ind-2,nums,dp);
+    //         int notpick=0+func(ind-1,nums,dp);
+    //         return dp[ind]=max(pick,notpick);
+    //     }
+
+    
+
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int> dp(n,-1);
-        int z=func(n-1,nums,dp);
-        
-        return z;
+        int prev=nums[0];
+        int prev2=0;
+        for(int i=1;i<n;i++){
+            int take=nums[i];
+            if(i>1)take+=prev2;
+            int notake=0+prev;
+            int cur_i=max(take,notake);
+            prev2=prev;
+            prev=cur_i;
+        }
+        return prev;
         
     }
-    int func(int ind,vector<int>&nums,vector<int>&dp){
-                                                     
-            if(ind==0)return nums[0];
-            if(ind<0)return 0; 
-            if(dp[ind]!=-1)return dp[ind];                                                                                                                                                                                                                                                                                                                                         
-            int pick=nums[ind]+func(ind-2,nums,dp);
-            int notpick=0+func(ind-1,nums,dp);
-            return dp[ind]=max(pick,notpick);
-        }
+
 };
