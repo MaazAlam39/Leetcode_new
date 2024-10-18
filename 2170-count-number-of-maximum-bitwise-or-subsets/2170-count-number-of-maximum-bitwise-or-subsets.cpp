d09f32e284235d474cb0251f81a9087b24a48634
Maaz <102986485+MaazAlam39@users.noreply.github.com>
2024-10-18 20:55:41 +0530

    Time: 195 ms (11.36%), Space: 309.8 MB (6.76%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
-
-    int func(int i,int ORS,int maxi,vector<int>&nums){
-        if(i>=nums.size())return (ORS==maxi)?1:0;
-        int cnt=0;
-        cnt+=func(i+1,ORS | nums[i],maxi,nums);//pick
-        cnt+=func(i+1,ORS,maxi,nums);//not pick
-        return cnt;
+    vector<vector<int>> dp;
+    int func(int i, int ORS, int maxi, vector<int>& nums) {
+        if (i >= nums.size()) {
+            return (ORS == maxi) ? 1 : 0;
+        }
+        if (dp[i][ORS] != -1) {
+            return dp[i][ORS];
+        }
+        int cnt = 0;
+        cnt += func(i + 1, ORS | nums[i], maxi, nums); // pick
+        cnt += func(i + 1, ORS, maxi, nums); // not pick
+        return dp[i][ORS] = cnt;
     }
 
     int countMaxOrSubsets(vector<int>& nums) {
-        int n=nums.size();
-        int maxi=0;
-        for(int i=0;i<nums.size();i++){
-            maxi |=nums[i];
+        int n = nums.size();
+        int maxi = 0;
+        for (int i = 0; i < n; i++) {
+            maxi |= nums[i];
         }
-        return func(0,0,maxi,nums);
+        dp.assign(n, vector<int>((1 << 17), -1)); // 17 bits assuming nums[i] is within 17 bits
+        return func(0, 0, maxi, nums);
     }
 };
